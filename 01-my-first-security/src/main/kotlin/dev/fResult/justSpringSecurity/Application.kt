41bd03a3ba0e38059981c8cd09b23168e6259080
fResult <styxmaz@gmail.com>
2025-01-20 00:33:14 +0700

    Prepare module `01-my-first-security`.

--- 01-my-first-security/src/main/kotlin/dev/fResult/justSpringSecurity/Application.kt
+++ 01-my-first-security/src/main/kotlin/dev/fResult/justSpringSecurity/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class JustSpringSecurityApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<JustSpringSecurityApplication>(*args)
+	runApplication<Application>(*args)
 }
