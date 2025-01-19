153679465d8346fc8740945675367913c496db3f
fResult <styxmaz@gmail.com>
2025-01-20 00:58:02 +0700

    Gather modules.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,42 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version libs.versions.springboot.get()
-	id("io.spring.dependency-management") version "1.1.7"
+  kotlin("jvm") version "1.9.25"
+  kotlin("plugin.spring") version "1.9.25"
+  id("org.springframework.boot") version libs.versions.springboot.get()
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "dev.fResult"
 version = "0.0.1"
+description = "Practicing Spring Security with Kotlin"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+subprojects {
+  apply(plugin = "org.springframework.boot")
+  apply(plugin = "io.spring.dependency-management")
+  apply(plugin = "kotlin")
+  apply(plugin = "kotlin-spring")
+
+  dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+  }
+
+  tasks.withType<Test> {
+    useJUnitPlatform()
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-	developmentOnly(libs.spring.boot.devtools)
-
-	implementation(libs.spring.boot.starter.security)
-	implementation(libs.spring.boot.starter.web)
-	implementation(libs.spring.boot.starter.data.jpa)
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-	testImplementation(libs.spring.boot.starter.test)
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+  developmentOnly(libs.spring.boot.devtools)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
-rootProject.name = "justSpringSecurity"
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "JustSpringSecurity"
+include("01-my-first-security")
