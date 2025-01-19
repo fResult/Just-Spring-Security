8e3c9cc32cd9837d2089381631b4bf1d7abd8c5b
fResult <styxmaz@gmail.com>
2025-01-20 01:48:14 +0700

    Config `userDetailsService` bean in SecurityConfig.

--- 01-my-first-security/src/main/kotlin/dev/fResult/justSpringSecurity/SecurityConfig.kt
+++ 01-my-first-security/src/main/kotlin/dev/fResult/justSpringSecurity/SecurityConfig.kt
@@ -4,14 +4,27 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class SecurityConfig {
+  @Bean
+  fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
   @Bean
   fun initUsers(repository: UserManagementRepository): CommandLineRunner? {
     return CommandLineRunner { _ ->
       repository.save(UserAccount("user", "password", "ROLE_USER"))
       repository.save(UserAccount("admin", "password", "ROLE_ADMIN"))
+    }
+  }
+
+  @Bean
+  fun userService(userRepository: UserRepository): UserDetailsService {
+    return UserDetailsService { username: String ->
+      userRepository.findByUsername(username)?.asUser(passwordEncoder())
+        ?: throw NoSuchElementException("Username or Password is incorrect")
     }
   }
 }
