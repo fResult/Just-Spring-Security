1410c2979bf2b44f1c11094484e650f0b855ce68
fResult <styxmaz@gmail.com>
2025-01-20 11:57:12 +0700

    Create GET `/users?username` endpoint.

--- 01-my-first-security/src/main/kotlin/dev/fResult/justSpringSecurity/HomeController.kt
+++ 01-my-first-security/src/main/kotlin/dev/fResult/justSpringSecurity/HomeController.kt
@@ -3,13 +3,17 @@ package dev.fResult.justSpringSecurity
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
-class HomeController {
+class HomeController(val userRepository: UserRepository) {
   @GetMapping
-  fun home(): ResponseEntity<String> {
-    return ResponseEntity.ok("Hello, World!")
-  }
+  fun home(): ResponseEntity<String> =
+    ResponseEntity.ok("Hello, World!")
+
+  @GetMapping("/users")
+  fun getUser(@RequestParam username: String): ResponseEntity<UserInfo> = userRepository.findByUsername(username)
+    ?.let { ResponseEntity.ok(UserInfo.fromDao(it)) } ?: ResponseEntity.notFound().build()
 }
