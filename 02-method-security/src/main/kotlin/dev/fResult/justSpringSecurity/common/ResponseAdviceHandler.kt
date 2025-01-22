120efa586ea752f2ea3b79f6a90bc4136866376d
fResult <styxmaz@gmail.com>
2025-01-23 03:24:31 +0700

    Create controller advice class.

---
+++ 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/common/ResponseAdviceHandler.kt
@@ -0,0 +1,28 @@
+package dev.fResult.justSpringSecurity.common
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ResponseAdviceHandler {
+  @ExceptionHandler(NoSuchElementException::class)
+  protected fun handleNoSuchElementException(ex: NoSuchElementException): ResponseEntity<Any> {
+    val detail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message)
+    println("Warning: ${ex.message}")
+
+    return ResponseEntity.of(detail).build()
+  }
+
+  @ExceptionHandler(Exception::class)
+  protected fun handleGlobalException(ex: Exception): ResponseEntity<*> {
+    val detail =
+      ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.message)
+
+    ex.printStackTrace()
+
+    return ResponseEntity.of(detail).build<Any>()
+  }
+}
