7d7a4057e54a51047e784fb2c8110942e56a3c77
fResult <styxmaz@gmail.com>
2025-01-23 04:26:08 +0700

    Create `VideoService.initVideos` method.

--- 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoService.kt
+++ 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoService.kt
@@ -1,9 +1,33 @@
 package dev.fResult.justSpringSecurity.videos
 
+import jakarta.annotation.PostConstruct
 import org.springframework.stereotype.Service
 
 @Service
 class VideoService(private val videoRepository: VideoRepository) {
+  @PostConstruct
+  fun initVideos(): Unit {
+    videoRepository.saveAll(
+      listOf(
+        VideoEntity(
+          "alice",
+          "Need HELP with your SPRING BOOT 3 App?",
+          "SPRING BOOT 3 will only speed things up and make it super SIMPLE to serve templates and raw data."
+        ),
+        VideoEntity(
+          "alice",
+          "Don't do THIS to your own CODE!",
+          "As a pro developer, never ever EVER do this to your code. Because you'll ultimately be doing it to YOURSELF!"
+        ),
+        VideoEntity(
+          "bob",
+          "SECRETS to fix BROKEN CODE!",
+          "Discover ways to not only debug your code, but to regain your confidence and get back in the game as a software developer."
+        )
+      )
+    )
+  }
+
   fun getVideos(): List<VideoEntity> {
     return videoRepository.findAll()
   }
