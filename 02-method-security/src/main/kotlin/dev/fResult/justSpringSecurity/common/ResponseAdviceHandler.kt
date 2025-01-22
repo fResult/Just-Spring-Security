c1dc037f56f3e6919d60d1917ecb9b5f6092d459
fResult <styxmaz@gmail.com>
2025-01-23 03:59:23 +0700

    Handle AccessDeniedException in controller advice.

--- 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/common/ResponseAdviceHandler.kt
+++ 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/common/ResponseAdviceHandler.kt
@@ -3,6 +3,7 @@ package dev.fResult.justSpringSecurity.common
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -11,6 +12,14 @@ class ResponseAdviceHandler {
   @ExceptionHandler(NoSuchElementException::class)
   protected fun handleNoSuchElementException(ex: NoSuchElementException): ResponseEntity<Any> {
     val detail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.message)
+    println("Warning: ${ex.message}")
+
+    return ResponseEntity.of(detail).build()
+  }
+
+  @ExceptionHandler(AccessDeniedException::class)
+  protected fun handleAccessDeniedException(ex: AccessDeniedException): ResponseEntity<Any> {
+    val detail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.message)
     println("Warning: ${ex.message}")
 
     return ResponseEntity.of(detail).build()
