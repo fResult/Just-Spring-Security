9eb650bcd25086c8cf4758da5b8628bbe3695101
fResult <styxmaz@gmail.com>
2025-01-23 03:50:39 +0700

    Pre authorize to delete video only for the one who created video.

--- 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/SecurityConfig.kt
+++ 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/SecurityConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.annotation.web.invoke
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
+@EnableMethodSecurity
 class SecurityConfig {
   @Bean
   fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
--- 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoRepository.kt
+++ 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoRepository.kt
@@ -1,5 +1,9 @@
 package dev.fResult.justSpringSecurity.videos
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.security.access.prepost.PreAuthorize
 
-interface VideoRepository : JpaRepository<VideoEntity, Long>
+interface VideoRepository : JpaRepository<VideoEntity, Long> {
+  @PreAuthorize("#entity.username == authentication.name")
+  override fun delete(entity: VideoEntity)
+}
