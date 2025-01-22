51f533e4de79070d0469207f19ad3d72b4416489
fResult <styxmaz@gmail.com>
2025-01-23 03:25:32 +0700

    Create DELETE `/videos` API endpoint.

--- 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoController.kt
+++ 02-method-security/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoController.kt
@@ -2,11 +2,7 @@ package dev.fResult.justSpringSecurity.videos
 
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 @RestController
@@ -21,4 +17,12 @@ class VideoController(private val videoService: VideoService) {
 
     return createdVideo.let { ResponseEntity.created(URI.create("/videos/${it.id}")).body(it) }
   }
+
+  @DeleteMapping("/{id}")
+  fun deleteVideo(@PathVariable id: Long): ResponseEntity<Void> = videoService.delete(id)
+    ?.let {
+      ResponseEntity.noContent().build()
+    }
+    ?: ResponseEntity.notFound().build()
 }
+
