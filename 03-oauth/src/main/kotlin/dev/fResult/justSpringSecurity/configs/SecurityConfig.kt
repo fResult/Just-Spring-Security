39a06bda0a2b436a7855aba35cb9b4906035df70
fResult <styxmaz@gmail.com>
2025-01-24 06:46:08 +0700

    Move `SecurityConfig` class into proper package.

--- 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/configs/SecurityConfig.kt
+++ 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/configs/SecurityConfig.kt
@@ -1,4 +1,4 @@
-package dev.fResult.justSpringSecurity
+package dev.fResult.justSpringSecurity.configs
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
