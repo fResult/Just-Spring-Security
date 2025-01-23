a5ba0b57f9fd250776d95e8b4f2581dd3209aa07
fResult <styxmaz@gmail.com>
2025-01-24 06:24:15 +0700

    Create `oAuth2AuthorizedClientManager` bean.

--- 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/SecurityConfig.kt
+++ 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/SecurityConfig.kt
@@ -1,3 +1,30 @@
 package dev.fResult.justSpringSecurity
 
-class SecurityConfig
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
+
+@Configuration
+class SecurityConfig {
+  @Bean
+  fun oAuth2AuthorizedClientManager(
+    clientRegRepository: ClientRegistrationRepository,
+    authClientRepository: OAuth2AuthorizedClientRepository
+  ): OAuth2AuthorizedClientManager {
+    val clientManager = DefaultOAuth2AuthorizedClientManager(clientRegRepository, authClientRepository)
+    val clientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+      .authorizationCode()
+      .refreshToken()
+      .clientCredentials()
+      .password()
+      .build()
+
+    clientManager.setAuthorizedClientProvider(clientProvider)
+
+    return clientManager
+  }
+}
