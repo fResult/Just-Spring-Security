252a51c0ec80aa74298bb6ed1543c0f8548f6f74
fResult <styxmaz@gmail.com>
2025-01-25 00:22:27 +0700

    Update java doc in `PageInfo` data class.

--- 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/youtube/PageInfo.kt
+++ 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/youtube/PageInfo.kt
@@ -1,3 +1,6 @@
 package dev.fResult.justSpringSecurity.youtube
 
+/**
+ * @see <a href="https://developers.google.com/youtube/v3/docs/videos/list">YouTube Data API - Videos: list</a>
+ */
 data class PageInfo(val totalResults: Int, val resultsPerPage: Int)
