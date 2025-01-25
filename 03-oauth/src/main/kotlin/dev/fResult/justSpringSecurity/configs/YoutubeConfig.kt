d08fc0590f2bfef4a3bcb4fb6c6eb1b98807c34a
fResult <styxmaz@gmail.com>
2025-01-25 17:56:28 +0700

    Make `YoutubeConfig` support `youtube` bean.

--- 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/configs/YoutubeConfig.kt
+++ 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/configs/YoutubeConfig.kt
@@ -1,24 +1,38 @@
 package dev.fResult.justSpringSecurity.configs
 
+import dev.fResult.justSpringSecurity.youtube.Youtube
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.support.WebClientAdapter
+import org.springframework.web.service.invoker.HttpServiceProxyFactory
+
 
 @Configuration
 class YoutubeConfig {
   private companion object {
-    const val YOUTUBE_V3_API = "https://googleapis.com/youtube/v3"
+    const val YOUTUBE_V3_API = "https://www.googleapis.com/youtube/v3"
   }
 
   @Bean
   fun webClient(oAuth2AuthorizedClientManager: OAuth2AuthorizedClientManager): WebClient {
     val oAuth2 = ServletOAuth2AuthorizedClientExchangeFilterFunction(oAuth2AuthorizedClientManager)
 
+    oAuth2.setDefaultClientRegistrationId("google")
+
     return WebClient.builder()
       .baseUrl(YOUTUBE_V3_API)
       .apply(oAuth2.oauth2Configuration())
       .build()
   }
+
+  @Bean
+  fun proxyFactory(webClient: WebClient): HttpServiceProxyFactory =
+    HttpServiceProxyFactory.builder().exchangeAdapter(WebClientAdapter.create(webClient)).build()
+
+  @Bean
+  fun youtube(proxyFactory: HttpServiceProxyFactory): Youtube =
+    proxyFactory.createClient(Youtube::class.java)
 }
