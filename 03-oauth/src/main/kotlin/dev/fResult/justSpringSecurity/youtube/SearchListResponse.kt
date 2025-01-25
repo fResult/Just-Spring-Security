ce0c652168b07856f6cd731d0d2b8161ed642356
fResult <styxmaz@gmail.com>
2025-01-25 17:54:00 +0700

    Update DTO classes.

--- 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/youtube/SearchListResponse.kt
+++ 03-oauth/src/main/kotlin/dev/fResult/justSpringSecurity/youtube/SearchListResponse.kt
@@ -4,10 +4,10 @@ package dev.fResult.justSpringSecurity.youtube
  * @see <a href="https://developers.google.com/youtube/v3/docs/search/list#response">YouTube Data API - Search: list: Response</a>
  */
 data class SearchListResponse(
-  val kind: String,
-  val etag: String,
-  val nextPageToken: String,
-  val prevPageToken: String,
-  val pageInfo: PageInfo,
+  val kind: String?,
+  val etag: String?,
+  val nextPageToken: String?,
+  val prevPageToken: String?,
+  val pageInfo: PageInfo?,
   val items: List<SearchResult>
 )
