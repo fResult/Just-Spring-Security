6906e2a0839ce2ef2b39fa43c445bb6a442c4254
fResult <styxmaz@gmail.com>
2025-01-27 13:52:51 +0700

    Create `find all should produce all videos` test case.

--- 04-testing-with-junit5/src/test/kotlin/dev/fResult/justSpringSecurity/videos/VideoRepositoryTest.kt
+++ 04-testing-with-junit5/src/test/kotlin/dev/fResult/justSpringSecurity/videos/VideoRepositoryTest.kt
@@ -1,4 +1,28 @@
 package dev.fResult.justSpringSecurity.videos
 
-class VideoRepositoryTest {
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest
+class VideoRepositoryTest(@Autowired private val videoRepository: VideoRepository) {
+  @BeforeEach
+  fun setUp() {
+    val video1 = Video("Alice", "video 1", "description")
+    val video2 = Video("Alice", "video 2", "description")
+    val video3 = Video("Alice", "video 3", "description")
+
+    videoRepository.saveAll(listOf(video1, video2, video3))
+  }
+
+  @Test
+  fun `find all should produce all videos`() {
+    // When
+    val videos = videoRepository.findAll()
+
+    // Then
+    assertThat(videos).hasSize(3)
+  }
 }
