0228d2f79d1249997b001a96ab01ecbf8dd11abc
fResult <styxmaz@gmail.com>
2025-01-27 15:06:05 +0700

    Add new custom finder for `VideoRepository` interface.

--- 04-testing-with-junit5/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoRepository.kt
+++ 04-testing-with-junit5/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoRepository.kt
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.security.access.prepost.PreAuthorize
 
 interface VideoRepository : JpaRepository<Video, Long> {
+  fun findByNameContainsIgnoreCase(name: String): List<Video>
+
+  fun findByDescriptionContainsIgnoreCase(description: String): List<Video>
+
+  fun findByNameContainsOrDescriptionContainsAllIgnoreCase(name: String, description: String): List<Video>
+
   @PreAuthorize("#entity.username == authentication.name")
   override fun delete(entity: Video)
 }
