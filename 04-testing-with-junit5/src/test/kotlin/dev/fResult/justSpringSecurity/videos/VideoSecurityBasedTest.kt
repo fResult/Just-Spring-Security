731a04cc9bb86ee2f8162db2b1462eeb537e38e4
fResult <styxmaz@gmail.com>
2025-01-28 22:36:38 +0700

    Create `unauthenticated user shouldn't access videos` test case.

---
+++ 04-testing-with-junit5/src/test/kotlin/dev/fResult/justSpringSecurity/videos/VideoSecurityBasedTest.kt
@@ -0,0 +1,21 @@
+package dev.fResult.justSpringSecurity.videos
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(controllers = [VideoController::class])
+class VideoSecurityBasedTest(@Autowired val mockMvc: MockMvc) {
+  @MockitoBean
+  private lateinit var videoService: VideoService
+
+  @Test
+  fun `unauthenticated user shouldn't access videos`() {
+    mockMvc.perform(get("/videos")).andExpect(status().isUnauthorized)
+  }
+}
