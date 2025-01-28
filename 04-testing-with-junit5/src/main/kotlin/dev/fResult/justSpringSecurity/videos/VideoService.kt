09cbc0f3c6d0a83c40e1ffb71ba715cd7ac790a6
fResult <styxmaz@gmail.com>
2025-01-29 01:37:57 +0700

    Refactor `VideoService.initVideos` method with PostConstruct.

--- 04-testing-with-junit5/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoService.kt
+++ 04-testing-with-junit5/src/main/kotlin/dev/fResult/justSpringSecurity/videos/VideoService.kt
@@ -7,29 +7,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class VideoService(private val videoRepository: VideoRepository) {
-  @PostConstruct
-  fun initVideos(): Unit {
-    videoRepository.saveAll(
-      listOf(
-        Video(
-          "alice",
-          "Need HELP with your SPRING BOOT 3 App?",
-          "SPRING BOOT 3 will only speed things up and make it super SIMPLE to serve templates and raw data."
-        ),
-        Video(
-          "alice",
-          "Don't do THIS to your own CODE!",
-          "As a pro developer, never ever EVER do this to your code. Because you'll ultimately be doing it to YOURSELF!"
-        ),
-        Video(
-          "bob",
-          "SECRETS to fix BROKEN CODE!",
-          "Discover ways to not only debug your code, but to regain your confidence and get back in the game as a software developer."
-        )
-      )
-    )
-  }
-
   fun getVideos(search: String?): List<Video> = search
     ?.let(::convertToVideoExample)
     ?.let(videoRepository::findAll)
@@ -50,4 +27,26 @@ class VideoService(private val videoRepository: VideoRepository) {
     Video(search, search, search),
     ExampleMatcher.matchingAny().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING)
   )
+
+  @PostConstruct
+  fun initVideos(): Unit {
+    val video1 = Video(
+      "alice",
+      "Need HELP with your SPRING BOOT 3 App?",
+      "SPRING BOOT 3 will only speed things up and make it super SIMPLE to serve templates and raw data."
+    )
+    val video2 = Video(
+      "alice",
+      "Don't do THIS to your own CODE!",
+      "As a pro developer, never ever EVER do this to your code. Because you'll ultimately be doing it to YOURSELF!"
+    )
+    val video3 = Video(
+      "bob",
+      "SECRETS to fix BROKEN CODE!",
+      "Discover ways to not only debug your code, but to regain your confidence and get back in the game as a software developer."
+    )
+
+    videoRepository.saveAll(listOf(video1, video2, video3))
+  }
+
 }
