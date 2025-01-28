56c97c704b509bda3fd2a3701fb3dea7daf85626
fResult <styxmaz@gmail.com>
2025-01-29 03:07:40 +0700

    Create `new video should be forbidden for unauthenticated user` test case.

--- 04-testing-with-junit5/src/test/kotlin/dev/fResult/justSpringSecurity/videos/VideoSecurityBasedTest.kt
+++ 04-testing-with-junit5/src/test/kotlin/dev/fResult/justSpringSecurity/videos/VideoSecurityBasedTest.kt
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import kotlin.jvm.Throws
 
@@ -33,5 +35,13 @@ class VideoSecurityBasedTest(@Autowired val mockMvc: MockMvc) {
   @Throws(Exception::class)
   fun `admin user should access videos`() {
     mockMvc.perform(get("/videos")).andExpect(status().isOk)
+  }
+
+  @Test
+  @Throws(Exception::class)
+  fun `new video should be forbidden for unauthenticated user`() {
+    val requestBody = NewVideo("New Video", "New Video Description")
+    mockMvc.perform(post("/videos").with(csrf()))
+      .andExpect(status().isUnauthorized)
   }
 }
