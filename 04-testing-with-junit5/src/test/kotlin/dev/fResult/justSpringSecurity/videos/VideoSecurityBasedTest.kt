5cfa490928976a306811703966f492653180e953
fResult <styxmaz@gmail.com>
2025-01-30 01:27:19 +0700

    Create `new video from authenticated user should work` test case.

--- 04-testing-with-junit5/src/test/kotlin/dev/fResult/justSpringSecurity/videos/VideoSecurityBasedTest.kt
+++ 04-testing-with-junit5/src/test/kotlin/dev/fResult/justSpringSecurity/videos/VideoSecurityBasedTest.kt
@@ -1,19 +1,25 @@
 package dev.fResult.justSpringSecurity.videos
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import kotlin.jvm.Throws
 
 @WebMvcTest(controllers = [VideoController::class])
-class VideoSecurityBasedTest(@Autowired val mockMvc: MockMvc) {
+class VideoSecurityBasedTest(
+  @Autowired val mockMvc: MockMvc,
+  @Autowired val objectMapper: ObjectMapper,
+) {
   @MockitoBean
   private lateinit var videoService: VideoService
 
@@ -40,8 +46,25 @@ class VideoSecurityBasedTest(@Autowired val mockMvc: MockMvc) {
   @Test
   @Throws(Exception::class)
   fun `new video should be forbidden for unauthenticated user`() {
-    val requestBody = NewVideo("New Video", "New Video Description")
     mockMvc.perform(post("/videos").with(csrf()))
       .andExpect(status().isUnauthorized)
+  }
+
+  @Test
+  @WithMockUser(username = "Alice", roles = ["USER"])
+  @Throws(Exception::class)
+  fun `new video from authenticated user should work`() {
+    val requestBody = NewVideo("New Video", "New Video Description")
+    val createdVideo = Video(1, "Alice", "New Video", "New Video Description")
+
+    given(videoService.create(requestBody, "Alice")).willReturn(createdVideo)
+
+    mockMvc.perform(
+      post("/videos")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(requestBody))
+        .with(csrf())
+    ).andExpect(status().isCreated)
+      .andExpect(jsonPath("$.id").value(createdVideo.id))
   }
 }
