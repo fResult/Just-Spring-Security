5d7cd198e08bce07c58fcf257e1b174203931d90
fResult <styxmaz@gmail.com>
2025-01-30 20:55:28 +0700

    Make `AppConfigProperties` possible to inject to Spring Bean.

--- 05-configure-app/src/main/kotlin/dev/fResult/justSpringSecurity/Application.kt
+++ 05-configure-app/src/main/kotlin/dev/fResult/justSpringSecurity/Application.kt
@@ -1,9 +1,12 @@
 package dev.fResult.justSpringSecurity
 
+import dev.fResult.justSpringSecurity.common.configs.AppConfigProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppConfigProperties::class)
 class Application
 
 fun main(args: Array<String>) {
