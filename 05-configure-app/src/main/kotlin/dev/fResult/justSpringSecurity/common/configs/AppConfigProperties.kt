409b3577026c284cf90f7dbf7e49861b70d9e633
fResult <styxmaz@gmail.com>
2025-01-30 20:56:41 +0700

    Create `toDAO` method to map to DAO.

--- 05-configure-app/src/main/kotlin/dev/fResult/justSpringSecurity/common/configs/AppConfigProperties.kt
+++ 05-configure-app/src/main/kotlin/dev/fResult/justSpringSecurity/common/configs/AppConfigProperties.kt
@@ -6,7 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "app.config")
 data class AppConfigProperties(val users: List<UserAccount>) {
   companion object {
-    data class UserAccount(val username: String, val password: String, val roles: List<UserRole>)
+    data class UserAccount(val username: String, val password: String, val roles: List<UserRole>) {
+      fun toDAO() = UserAccount(null, username, password, roles.map { it.role })
+    }
 
     enum class UserRole(val role: String) {
       ADMIN("ROLE_ADMIN"),
