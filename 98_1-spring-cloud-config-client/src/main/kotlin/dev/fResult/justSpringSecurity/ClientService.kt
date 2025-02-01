6b4f9c786f044af9abac15d92cb9f7e0300018a6
fResult <styxmaz@gmail.com>
2025-02-02 05:23:39 +0700

    Add javadoc for `@Value("${my.property}") myProperty`.

--- 98_1-spring-cloud-config-client/src/main/kotlin/dev/fResult/justSpringSecurity/ClientService.kt
+++ 98_1-spring-cloud-config-client/src/main/kotlin/dev/fResult/justSpringSecurity/ClientService.kt
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service
 
 @Service
 class ClientService {
+  /**
+   * `my.property` is the application property which is stored in [GitHub Repository](https://github.com/fResult/Spring-Configuration-Externalization)
+   */
   @Value("\${my.property}")
   private val myProperty: String? = null
 
